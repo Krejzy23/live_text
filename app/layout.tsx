c13cc81f00844a6212a193b93233fd23d4c354a6
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-21 17:49:20 +0200

    add liveblocks funcionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,9 +26,9 @@ export default function RootLayout({
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { 
+        variables: {
           colorPrimary: "#3371FF",
-          fontSize: '16px',
+          fontSize: "16px",
         },
       }}
     >
@@ -39,7 +40,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>

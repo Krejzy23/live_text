2892f8b288c5f205fdcf91658aee0cef9ef676ea
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-22 19:30:47 +0200

    add delete funcionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,6 +8,7 @@ import { redirect } from 'next/navigation'
 import { getDocuments } from '@/lib/actions/room.actions'
 import Link from 'next/link'
 import { dateConverter } from '@/lib/utils'
+import { DeleteModal } from '@/components/DeleteModal'
 
 
 const Home = async () => {
@@ -55,6 +56,7 @@ const Home = async () => {
                     <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                   </div>
                 </Link>
+                <DeleteModal roomId={id} />
               </li>
             ))}
           </ul>
